6b4020aaa518388abe3b9a3231d691dea24f6a9b
shahdaksh0605 <shah.daksh333@gmail.com>
2025-01-04 21:43:32 +0530

    day-1

--- Main/Function.js
+++ Main/Function.js
@@ -1,22 +1,22 @@
-// window.addEventListener('load', () => {
-//     const animation = document.getElementById('animation');
-//     const welcomeMessage = document.getElementById('welcome-message');
-//     const mainContent = document.getElementById('main-content');
+window.addEventListener('load', () => {
+    const animation = document.getElementById('animation');
+    const welcomeMessage = document.getElementById('welcome-message');
+    const mainContent = document.getElementById('main-content');
 
-//     // Step 1: End plane animation
-//     setTimeout(() => {
-//         welcomeMessage.classList.add('show');
-//     }, 500); // Plane animation duration
+    // Step 1: End plane animation
+    setTimeout(() => {
+        welcomeMessage.classList.add('show');
+    }, 500); // Plane animation duration
 
-//     // Step 2: Fade out welcome message
-//     setTimeout(() => {
-//         welcomeMessage.classList.remove('show');
-//     }, 5000); // Duration to show welcome message
+    // Step 2: Fade out welcome message
+    setTimeout(() => {
+        welcomeMessage.classList.remove('show');
+    }, 5000); // Duration to show welcome message
 
-//     // Step 3: Hide animation container and show main content
-//     setTimeout(() => {
-//         animation.style.display = 'none';
-//         mainContent.classList.add('visible');
-//         document.body.style.overflow = 'auto';
-//     }, 3000); // Total delay before showing main content
-// });
+    // Step 3: Hide animation container and show main content
+    setTimeout(() => {
+        animation.style.display = 'none';
+        mainContent.classList.add('visible');
+        document.body.style.overflow = 'auto';
+    }, 3000); // Total delay before showing main content
+});
