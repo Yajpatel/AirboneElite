ea8f0e0b9f351b27751b4a6d2af0fcdf29b1d366
Yajpatel <yajpatel3594@gmail.com>
2025-02-09 18:19:58 +0530

    jets page almost done now heading to details

---
+++ Main/Home/Function.js
@@ -0,0 +1,10 @@
+// function changeAircraft(image, info) {
+//     const display = document.getElementById('display');
+//     const infoBox = document.getElementById('info');
+//     display.style.opacity = 0;
+//     setTimeout(() => {
+//         display.src = image;
+//         display.style.opacity = 1;
+//         infoBox.textContent = info;
+//     }, 300);
+// }
